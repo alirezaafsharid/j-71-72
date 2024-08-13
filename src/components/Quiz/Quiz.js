f739f87ace6f60a500e890914c69ac7f1d64c08a
alireza <afshar2451382@gmail.com>
2024-08-13 11:38:08 +0330

    اتمام پروژه کوییز

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,76 +1,109 @@
-import React from 'react';
-import './Quiz.css'
+import React from "react";
+import "./Quiz.css";
+import { ImPrevious2 } from "react-icons/im";
 
 export default class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      questions: [
+        {
+          questionText: "What is the capital of France?",
+          answerOptions: [
+            { answerText: "New York", isCorrect: false },
+            { answerText: "London", isCorrect: false },
+            { answerText: "Paris", isCorrect: true },
+            { answerText: "Dublin", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Who is CEO of Tesla?",
+          answerOptions: [
+            { answerText: "Jeff Bezos", isCorrect: false },
+            { answerText: "Elon Musk", isCorrect: true },
+            { answerText: "Bill Gates", isCorrect: false },
+            { answerText: "Tony Stark", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "The iPhone was created by which company?",
+          answerOptions: [
+            { answerText: "Apple", isCorrect: true },
+            { answerText: "Intel", isCorrect: false },
+            { answerText: "Amazon", isCorrect: false },
+            { answerText: "Microsoft", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "How many Harry Potter books are there?",
+          answerOptions: [
+            { answerText: "1", isCorrect: false },
+            { answerText: "4", isCorrect: false },
+            { answerText: "6", isCorrect: false },
+            { answerText: "7", isCorrect: true },
+          ],
+        },
+      ],
+      currentQuestion: 0,
+      showScore: false,
+      score: 0,
 
-        this.state = {
-            questions: [
-                {
-                    questionText: 'What is the capital of France?',
-                    answerOptions: [
-                        { answerText: 'New York', isCorrect: false },
-                        { answerText: 'London', isCorrect: false },
-                        { answerText: 'Paris', isCorrect: true },
-                        { answerText: 'Dublin', isCorrect: false },
-                    ],
-                },
-                {
-                    questionText: 'Who is CEO of Tesla?',
-                    answerOptions: [
-                        { answerText: 'Jeff Bezos', isCorrect: false },
-                        { answerText: 'Elon Musk', isCorrect: true },
-                        { answerText: 'Bill Gates', isCorrect: false },
-                        { answerText: 'Tony Stark', isCorrect: false },
-                    ],
-                },
-                {
-                    questionText: 'The iPhone was created by which company?',
-                    answerOptions: [
-                        { answerText: 'Apple', isCorrect: true },
-                        { answerText: 'Intel', isCorrect: false },
-                        { answerText: 'Amazon', isCorrect: false },
-                        { answerText: 'Microsoft', isCorrect: false },
-                    ],
-                },
-                {
-                    questionText: 'How many Harry Potter books are there?',
-                    answerOptions: [
-                        { answerText: '1', isCorrect: false },
-                        { answerText: '4', isCorrect: false },
-                        { answerText: '6', isCorrect: false },
-                        { answerText: '7', isCorrect: true },
-                    ],
-                },
-            ],
-            currentQuestion: 0,
-            showScore: false,
-            score: 0
-        }
+      //   this.clickHandler = this.clickHandler.bind(this)
+    };
+  }
+  clickHandler(isCorrect) {
+    if (isCorrect) {
+      this.setState((prevState) => {
+        return {
+          score: prevState.score + 1,
+        };
+      });
     }
+    if (this.state.currentQuestion === 3) {
+      this.setState({ showScore: true });
+    } else {
+      this.setState((prevState) => {
+        return {
+          currentQuestion: prevState.currentQuestion + 1,
+        };
+      });
+    }
+  }
 
-    render() {
-        return (
-            <div className='app'>
-                {/* next div is for showing user score */}
-                    {/* <div className='score-section'>
-                        You scored 0 out of 4
-                    </div> */}
-                        <div className='question-section'>
-                            <div className='question-count'>
-                                <span>Question 1</span>/ 4
-                            </div>
-                            <div className='question-text'>Where is Iran capital?</div>
-                        </div>
-                        <div className='answer-section'>
-                                <button>Tehran</button>
-                                <button>Tabriz</button>
-                                <button>Gorgan</button>
-                                <button>Shiraz</button>
-                        </div>
+  render() {
+    return (
+      <div className="app">
+        {/* next div is for showing user score */}
+        {this.state.showScore ? (
+          <div className="score-section">
+            You scored {this.state.score} out of {this.state.questions.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {this.state.currentQuestion + 1} </span>/
+                {this.state.questions.length}
+              </div>
+              <div className="question-text">
+                {this.state.questions[this.state.currentQuestion].questionText}
+              </div>
             </div>
-        )
-    }
+            <div className="answer-section">
+              {this.state.questions[
+                this.state.currentQuestion
+              ].answerOptions.map((answer) => (
+                <button
+                  onClick={this.clickHandler.bind(this, answer.isCorrect)}
+                >
+                  {answer.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
 }
